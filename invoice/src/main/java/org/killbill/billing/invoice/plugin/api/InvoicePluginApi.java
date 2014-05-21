4102d616a1f0e9d692a691ebd672c207060bcc68
val <valentin.crettaz@consulthys.com>
2014-05-21 23:20:24 +0200

    Added new plugin API for hooking into invoice processing

--- invoice/src/main/java/org/killbill/billing/invoice/plugin/api/InvoicePluginApi.java
+++ invoice/src/main/java/org/killbill/billing/invoice/plugin/api/InvoicePluginApi.java
@@ -4,20 +4,16 @@ import java.util.List;
 
 import org.killbill.billing.invoice.api.Invoice;
 import org.killbill.billing.invoice.api.InvoiceItem;
+import org.killbill.billing.payment.api.PluginProperty;
+import org.killbill.billing.util.callcontext.CallContext;
 
 public interface InvoicePluginApi {
 
     /**
-     * Returns additional external charge items (setup fees, etc) to be added to the invoice upon creation
+     * Returns additional invoice items (setup fees, VAT tax, etc) to be added to the invoice upon creation
      * @param invoice the invoice that is being created
-     * @return the list of additional external charge items to add to the new invoice
+     * @param context the call context
+     * @return the list of additional invoice items to add to the new invoice
      */
-    List<InvoiceItem> getAdditionalExternalChargeItems(Invoice invoice);
-
-    /**
-     * Returns tax items (VAT, etc) to be added to the invoice upon creation
-     * @param invoice the invoice that is being created
-     * @return the list of tax items to add to the new invoice
-     */
-    List<InvoiceItem> getAdditionalTaxItems(Invoice invoice);
+    List<InvoiceItem> getAdditionalInvoiceItems(Invoice invoice, Iterable<PluginProperty> properties, CallContext context);
 }
