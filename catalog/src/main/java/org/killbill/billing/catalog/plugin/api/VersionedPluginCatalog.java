9cc87e4a556f8fac8237cb71b241386de4d8cc2c
stephane brossier <stephane@ning.com>
2015-06-19 16:03:15 -0700

    Initial proposal for catalog plugin api

---
+++ catalog/src/main/java/org/killbill/billing/catalog/plugin/api/VersionedPluginCatalog.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2010-2013 Ning, Inc.
+ * Copyright 2015 Groupon, Inc
+ * Copyright 2015 The Billing Project, LLC
+ *
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.killbill.billing.catalog.plugin.api;
+
+import org.killbill.billing.catalog.api.BillingMode;
+
+public interface VersionedPluginCatalog {
+
+    /**
+     *
+     * @return the name of the catalog
+     */
+    public String getCatalogName();
+
+    /**
+     *
+     * @return the {@code BillingMode} for that catalog
+     */
+    public BillingMode getRecurringBillingMode();
+
+    /**
+     *
+     * @return the {@code StandalonePluginCatalog} for each version
+     */
+    public Iterable<StandalonePluginCatalog> getStandalonePluginCatalogs();
+}
