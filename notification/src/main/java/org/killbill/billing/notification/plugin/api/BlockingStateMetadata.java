c4c48a684603d81822e8dd08c11d8d7262dd71c4
stephane brossier <sbrossier@groupon.com>
2016-02-19 12:50:28 -0800

    Add metadata object to serialize BlockingState info into public bus bus event

---
+++ notification/src/main/java/org/killbill/billing/notification/plugin/api/BlockingStateMetadata.java
@@ -0,0 +1,82 @@
+package org.killbill.billing.notification.plugin.api;
+
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+import org.killbill.billing.entitlement.api.BlockingStateType;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BlockingStateMetadata {
+
+    private UUID blockableId;
+    private String service;
+    private String stateName;
+    private BlockingStateType blockingType;
+    private DateTime effectiveDate;
+    private Boolean isTransitionedToBlockedBilling;
+    private Boolean isTransitionedToUnblockedBilling;
+    private Boolean isTransitionedToBlockedEntitlement;
+    private Boolean isTransitionedToUnblockedEntitlement;
+
+    public BlockingStateMetadata() {
+    }
+
+    @JsonCreator
+    public BlockingStateMetadata(@JsonProperty("blockableId") final UUID blockableId,
+                                 @JsonProperty("service") final String service,
+                                 @JsonProperty("stateName") final String stateName,
+                                 @JsonProperty("blockingType") final BlockingStateType blockingType,
+                                 @JsonProperty("effectiveDate") final DateTime effectiveDate,
+                                 @JsonProperty("isTransitionedToBlockedBilling") final Boolean isTransitionedToBlockedBilling,
+                                 @JsonProperty("isTransitionedToUnblockedBilling") final Boolean isTransitionedToUnblockedBilling,
+                                 @JsonProperty("isTransitionedToBlockedEntitlement") final Boolean isTransitionedToBlockedEntitlement,
+                                 @JsonProperty("isTransitionedToUnblockedEntitlement") final Boolean isTransitionedToUnblockedEntitlement) {
+        this.blockableId = blockableId;
+        this.service = service;
+        this.stateName = stateName;
+        this.blockingType = blockingType;
+        this.effectiveDate = effectiveDate;
+        this.isTransitionedToBlockedBilling = isTransitionedToBlockedBilling;
+        this.isTransitionedToUnblockedBilling = isTransitionedToUnblockedBilling;
+        this.isTransitionedToBlockedEntitlement = isTransitionedToBlockedEntitlement;
+        this.isTransitionedToUnblockedEntitlement = isTransitionedToUnblockedEntitlement;
+    }
+
+    public UUID getBlockableId() {
+        return blockableId;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public BlockingStateType getBlockingType() {
+        return blockingType;
+    }
+
+    public DateTime getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public Boolean getTransitionedToBlockedBilling() {
+        return isTransitionedToBlockedBilling;
+    }
+
+    public Boolean getTransitionedToUnblockedBilling() {
+        return isTransitionedToUnblockedBilling;
+    }
+
+    public Boolean getTransitionedToBlockedEntitlement() {
+        return isTransitionedToBlockedEntitlement;
+    }
+
+    public Boolean getTransitionedToUnblockedEntitlement() {
+        return isTransitionedToUnblockedEntitlement;
+    }
+}
